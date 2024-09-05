103f6c09e7ddaea925bc93c2e166faf459a5077d
Izushi <shoeshinesamurai@gmail.com>
2024-09-04 20:50:20 -0700

    useState to manage state

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,17 @@
-import { Content, Title } from "./";
+import { useState } from "react";
+import { Title, Content, PublishButton } from "./";
 
 const Article = (props) => {
+  const [isPublished, setIsPublished] = useState(false)
+  const publishArticle = () => {
+    setIsPublished(!isPublished)
+  }
+  console.log(isPublished)
   return (
     <div>
       <Title title={props.title} />
       <Content content={props.content} />
+      <PublishButton isPublished={isPublished} onClick={publishArticle} />
     </div>
   );
 };
