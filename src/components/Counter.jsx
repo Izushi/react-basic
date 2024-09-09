feda2282a15cc83c371351fb2014a045f34a4b11
Izushi <shoeshinesamurai@gmail.com>
2024-09-08 20:35:47 -0700

    useEffect Life cycle and Effect

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0)
@@ -14,6 +14,10 @@ const Counter = () => {
       setCount(prevState => prevState - 1)
     }, 1000)
   }
+
+  useEffect(() => {
+    console.log('Current count', count)
+  }, [count])
 
   return (
     <div>
--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 const ToggleButton = () => {
   const [open, setOpen] = useState(false)
+
   const handleOpen = () => {
     setOpen(prevState => !prevState)
   }
+
+  useEffect(() => {
+    console.log('Current state is', open)
+    if (open) {
+      console.log('Subscribe database')
+    }
+    return () => {
+      console.log('Unsubscribe database')
+    }
+  }, [open])
+
   return (
     <button onClick={handleOpen}>
       {open ? 'Open' : 'Close'}
