a5c6ce72c5e2be166f7301638a967858dfd0ccc7
Izushi <shoeshinesamurai@gmail.com>
2024-09-09 15:32:56 -0700

    API with useEffect

--- src/App.js
+++ src/App.js
@@ -4,20 +4,49 @@ import Counter from "./components/Counter";
 import BadCounter from "./components/BadCounter";
 import ToggleButton from "./components/ToggleButton";
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  // return (
+  //   <div>
+  //     <Article
+  //       title={'新・日本一分かりやすいReact入門'}
+  //       content={'importとexportを使いこなそう'}
+  //     />
+  //     <TextInput />
+  //     <Counter />
+  //     <BadCounter />
+  //     <ToggleButton />
+  //   </div>
+  // );
+  const [name, setName] = useState('')
+  const [id, setId] = useState('Izushi')
+  const ids = ['deatiger', 'gaearon', 'aws', 'google', 'facebook']
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setName(data.name)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [id])
+
   return (
     <div>
-      <Article
-        title={'新・日本一分かりやすいReact入門'}
-        content={'importとexportを使いこなそう'}
-      />
-      <TextInput />
-      <Counter />
-      <BadCounter />
-      <ToggleButton />
+      <h1>GitHub User Info</h1>
+      <p>ID: {id}</p>
+      <p>Name: {name}</p>
+      <button onClick={getRandomId}>IDを変更</button>
     </div>
-  );
+  )
 }
 
 export default App;
